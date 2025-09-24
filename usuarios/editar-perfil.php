12963086392cd20815b46e10684be4eaeb113d10
Juanpe <juanpedrogomezespinosa@gmail.com>
2025-09-24 13:27:31 +0200

    editar perfil corregido

--- usuarios/editar-perfil.php
+++ usuarios/editar-perfil.php
@@ -28,31 +28,35 @@ include __DIR__ . '/../templates/header.php';
     <div class="container-agregar">
         <h1>Editar perfil</h1>
 
-        <form action="procesar-editar-perfil.php" method="POST" enctype="multipart/form-data" class="form-vertical">
-            <!-- Nombre -->
-            <div class="form-group">
-                <label for="nombre">Nombre de usuario:</label>
-                <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($usuario['nombre'], ENT_QUOTES, 'UTF-8'); ?>" required>
-            </div>
-
-            <!-- Biografía -->
-            <div class="form-group">
-                <label for="biografia">Biografía:</label>
-                <textarea name="biografia" id="biografia" maxlength="300" placeholder="Escribe algo sobre ti..."><?php echo htmlspecialchars($biografia, ENT_QUOTES, 'UTF-8'); ?></textarea>
-            </div>
-
+        <form action="procesar-editar-perfil.php" method="POST" enctype="multipart/form-data" class="form-horizontal">
+            
             <!-- Imagen actual -->
-            <div class="form-group">
+            <div class="columna izquierda">
                 <label>Imagen de perfil actual:</label>
                 <img src="<?php echo APP_URL . 'img/avatars/' . htmlspecialchars($avatar, ENT_QUOTES, 'UTF-8'); ?>" 
                      alt="Avatar de <?php echo htmlspecialchars($usuario['nombre'], ENT_QUOTES, 'UTF-8'); ?>" 
-                     width="120" style="border-radius:50%; border:2px solid #f4bf2c;">
+                     class="avatar-actual">
+            </div>
+
+            <!-- Nombre + Biografía -->
+            <div class="columna centro">
+                <div class="form-group">
+                    <label for="nombre">Nombre de usuario:</label>
+                    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($usuario['nombre'], ENT_QUOTES, 'UTF-8'); ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="biografia">Biografía:</label>
+                    <textarea name="biografia" id="biografia" maxlength="300" placeholder="Escribe algo sobre ti..."><?php echo htmlspecialchars($biografia, ENT_QUOTES, 'UTF-8'); ?></textarea>
+                </div>
             </div>
 
             <!-- Nueva imagen -->
-            <div class="form-group">
-                <label for="avatar">Nueva imagen de perfil:</label>
-                <input type="file" name="avatar" id="avatar" accept="image/*">
+            <div class="columna derecha">
+                <div class="form-group">
+                    <label for="avatar">Nueva imagen de perfil:</label>
+                    <input type="file" name="avatar" id="avatar" accept="image/*">
+                </div>
             </div>
 
             <!-- Botón -->
